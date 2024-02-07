ec4658a17d69fb80a55787518c580e476159aabd
Cristian <139021004+Cbaca4@users.noreply.github.com>
2024-02-06 19:58:34 -0600

    added color and text to login

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -12,11 +12,14 @@ const Header = () => {
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <Link className="text-dark" to="/">
           <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Tech Friends
+            Narnes and Bobles
           </h1>
         </Link>
         <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-          Meet your new programming pals.
+        Totally not a knock off.
+        </p>
+        <p className="m-0" style={{ fontSize: '1.00rem', fontWeight: '700' }}>
+        No really..
         </p>
         <div>
           {Auth.loggedIn() ? (
